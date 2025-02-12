e68de065902e62823033a876cfb9ad8c8e000a69
jason.venable@gmail.com <jason.venable@gmail.com>
2025-02-12 14:19:04 -0500

    add command line options

--- README.md
+++ README.md
@@ -24,26 +24,39 @@ This project connects to Salesforce and listens for Account Change Events using 
 
 ## Configuration
 1. Create a `.env` file in the root directory of the project.
-2. Add the following environment variables to the `.env` file:
+2. Add the following environment variables to the `.env` 
+
+For username password login:
    ```env
    SF_USERNAME=<your-salesforce-username>
    SF_PASSWORD=<your-salesforce-password>
    SF_SECURITY_TOKEN=<your-salesforce-security-token>
-   SF_LOGIN_URL=<your-salesforce-login-url>
+   SF_LOGIN_URL=https://login.salesforce.com or https://test.salesforce.com
+   ```
+   For client credentials login flow:
+   ```
+   SF_CLIENT_ID=<connected-app-clientId>
+   SF_CLIENT_SECRET=<connected-app-clientSecret>
+   SF_DOMAIN=<instance-url>
    ```
 
 ## Usage
-1. To start the project, run the following command:
-   ```sh
-   npm start
-   ```
-2. The project will log in to Salesforce and subscribe to the Account Change Events channel.
-3. You can filter the events by setting the `recordIdFilter` variable in the `cdc.js` file:
-   ```javascript
-   // ...existing code...
-   const recordIdFilter = "0015a00003K9pycAAB"; // Set your desired record ID
-   // ...existing code...
-   ```
+
+```
+node cdc.js --channel <channel name> --recordId <record id to filter>
+```
+```
+node cdc.js --channel AccountChangeEvent
+```
+```
+node cdc.js --channel AccountChangeEvent --recordId 0015a00003SWRhFWFW
+```
+```
+node cdc.js --channel ContractChangeEvent
+```
+```
+node cdc.js --channel ContractChangeEvent --recordId 8005a000005DE54FDG
+```
 
 ## License
 This project is licensed under the MIT License.
--- cdc.js
+++ cdc.js
@@ -1,25 +1,67 @@
 import jsforce from "jsforce";
-import { colorize } from "json-colorizer";
-
+import axios from "axios";
+import colorize from "json-colorizer";
+import { parseArgs } from "node:util";
 import "dotenv/config";
 
 const SF_USERNAME = process.env.SF_USERNAME;
 const SF_PASSWORD = process.env.SF_PASSWORD;
 const SF_SECURITY_TOKEN = process.env.SF_SECURITY_TOKEN;
 const SF_LOGIN_URL = process.env.SF_LOGIN_URL;
+const SF_INSTANCE_URL = process.env.SF_INSTANCE_URL;
+const SF_CLIENT_ID = process.env.SF_CLIENT_ID;
+const SF_CLIENT_SECRET = process.env.SF_CLIENT_SECRET;
 
-// Channel for Account Change Events
-const channel = "/data/AccountChangeEvent";
+// Parse command line arguments
+const {
+  values: { channel, recordId },
+} = parseArgs({
+  options: {
+    channel: {
+      type: "string",
+      short: "c",
+      default: "AccountChangeEvent",
+    },
+    recordId: {
+      type: "string",
+      short: "i",
+    },
+  },
+});
 
-// Events are still delivered for all, but this will filter console logs
-const recordIdFilter = "001xxxxxxxxx";
-// const recordIdFilter = null;
+// SFDC connection
+let conn;
 
-// Create a new connection
-const conn = new jsforce.Connection({ loginUrl: SF_LOGIN_URL });
+// Log in to Salesforce with username, password and security token
+if (SF_USERNAME && SF_PASSWORD && SF_SECURITY_TOKEN) {
+  conn = new jsforce.Connection({ loginUrl: SF_LOGIN_URL });
+  await conn.login(SF_USERNAME, SF_PASSWORD + SF_SECURITY_TOKEN);
+}
+
+// Log in to Salesforce with client credentials flow
+if (SF_CLIENT_ID && SF_CLIENT_SECRET && SF_INSTANCE_URL) {
+  const tokenResponse = await axios({
+    method: "post",
+    url: SF_INSTANCE_URL + "/services/oauth2/token",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    data: new URLSearchParams({
+      grant_type: "client_credentials",
+      client_id: process.env.SF_CLIENT_ID, // your connected app's Consumer Key
+      client_secret: process.env.SF_CLIENT_SECRET, // your connected app's Consumer Secret
+    }),
+  });
+
+  // 2. Extract token and instance info
+  const { access_token, instance_url } = tokenResponse.data;
+
+  // 3. Initialize a jsforce Connection
+  conn = new jsforce.Connection({
+    instanceUrl: instance_url,
+    accessToken: access_token,
+  });
+}
 
 // Log in to Salesforce
-await conn.login(SF_USERNAME, SF_PASSWORD + SF_SECURITY_TOKEN);
 const user = await conn.identity();
 
 console.log("Successfully logged in with Username:", user.username);
@@ -27,13 +69,17 @@ console.log("Successfully logged in with Username:", user.username);
 let count = 0;
 
 // Subscribe to the streaming channel
-conn.streaming.topic(channel).subscribe((message) => {
-  if (!recordIdFilter || (recordIdFilter && message.payload.ChangeEventHeader.recordIds.includes(recordIdFilter))) {
+conn.streaming.topic("/data/" + channel).subscribe((message) => {
+  if (!recordId || (recordId && message.payload.ChangeEventHeader.recordIds.includes(recordId))) {
     console.log("--- AccountChangeEvent Received ---");
-    console.log(colorize(message));
+    console.log(colorize(JSON.stringify(message, null, 2)));
     count++;
     console.log(`Total messages received: ${count}`);
   }
 });
 
 console.log(`Subscribed to ${channel}`);
+
+if (recordId) {
+  console.log(`Filtering on RecordId: ${recordId}`);
+}
